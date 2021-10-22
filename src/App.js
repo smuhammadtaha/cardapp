977199d3751fc52a35256348d4f6660688bd19ff
Muhammad Taha <60589832+smuhammadtaha@users.noreply.github.com>
2021-10-22 23:43:57 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+import './App.css';
+import Card from './card'
+
+function App() {
+  let card = [
+   {
+     cardTitle:"My First Cart",
+     cardSubtitle:"MILE FAIL AS LEFT",
+     cardPara:"An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex.",
+     cardimg:"https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg"
+   },
+   {
+    cardTitle:"My First Cart",
+    cardSubtitle:"MILE FAIL AS LEFT",
+    cardPara:"An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex.",
+    cardimg:"https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg"
+  },
+  {
+    cardTitle:"My First Cart",
+    cardSubtitle:"MILE FAIL AS LEFT",
+    cardPara:"An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex.",
+    cardimg:"https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg"
+  }
+  ]
+
+// console.log(card.map((prop)=> prop.cardTitle))
+
+  return (
+    
+    <>
+    {card.map((item , index) => {
+      return(
+        <div key={index}>
+          <Card title={item.cardTitle} subtitle={item.cardSubtitle} img={item.cardimg} description={item.cardPara}  />
+        </div>
+      )
+
+    })}
+
+    {/* <Title {...card.map((prop)=>prop.cardTitle)} />
+    <Pera />
+    <Img/> */}
+    </>
+  );
+}
+
+export default App;
